2752c1f5ea64909e89bd223993618cf8229f4d16
Json <jsonlli255@gmail.com>
2021-03-26 21:15:55 +0100

    reset and modify duartion in Flight class model.

--- flights/models.py
+++ flights/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Flight(models.Model):
     origin = models.CharField(max_length=64)
     destination = models.CharField(max_length=64)
-    duration = models.IntegerField
+    duration = models.IntegerField()
